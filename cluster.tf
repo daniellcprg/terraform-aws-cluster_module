d046d2c4d8e0d3ee062928fe92933e8490e72a6e
Daniel Carvalho <daniellc.prg@gmail.com>
2022-05-03 15:44:30 -0300

    fix: remove provider

--- cluster.tf
+++ cluster.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.12.1"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_ecs_cluster" "cluster" {
   name = var.cluster_environment
 
