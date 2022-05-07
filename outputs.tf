9fec7a7cfad258b6ad171851f89f8f0d1ba34b16
Daniel Carvalho <daniellc.prg@gmail.com>
2022-05-07 14:49:20 -0300

    refactor: update output

--- outputs.tf
+++ outputs.tf
@@ -5,8 +5,8 @@ output "applications" {
         application_name = e.name
         application_port = e.port
         ecr_repository_url = aws_ecr_repository.main[i].repository_url
-        ecs_service_name = format("%s-%s", e.name, var.cluster_environment)
-        ecs_task_family = format("%s-%s", e.name, var.cluster_environment)
+        ecs_service_name = format("%s-%s-%s", e.name, e.type, var.cluster_environment)
+        ecs_task_family = format("%s-%s-%s", e.name, e.type, var.cluster_environment)
       }
     ]
   )
