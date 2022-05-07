9a5043eed234f258f337e6c234d66b4bda07c119
Daniel Carvalho <daniellc.prg@gmail.com>
2022-05-07 15:16:37 -0300

    fix: minor fix

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "applications" {
   value = tolist(
     [for i, e in var.applications : 
       {
-        application_name = e.name
+        application_name = format("%s-%s", e.name, e.type)
         application_port = e.port
         ecr_repository_url = aws_ecr_repository.main[i].repository_url
         ecs_service_name = format("%s-%s-%s", e.name, e.type, var.cluster_environment)
